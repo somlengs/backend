1b4ae1d5d3fcea94400c3e03f01308ee88bfea5d
HoutManut <huotmanut0@gmail.com>
2025-11-06 20:09:02 +0700

    Partial processing implementation

--- app/shared/services/project_processor.py
+++ app/shared/services/project_processor.py
@@ -1,19 +1,23 @@
 from __future__ import annotations
 
 import asyncio
-import uuid
+from uuid import UUID
 
 from sqlalchemy.orm import Session
+from fastapi.exceptions import HTTPException
 
 from app.entities.models.project import ProjectTable
+from app.entities.repositories.project.base import ProjectRepo
+from app.entities.schemas.internals.task import ProcessingTask
+from app.entities.types.enums.processing_status import ProcessingStatus
 
 
 class ProjectProcessor:
     instance: ProjectProcessor
+    tasks: dict[UUID, ProcessingTask]
 
     def __init__(self) -> None:
-        self.tasks: dict[str, asyncio.Task] = {}
-        self.listeners: dict[str, list[asyncio.Queue[str]]] = {}
+        self.tasks: dict[UUID, ProcessingTask] = {}
 
     @classmethod
     def init(cls):
@@ -21,14 +25,15 @@ class ProjectProcessor:
 
     async def process_task(
         self,
-        project_id: uuid.UUID,
-        user_id: uuid.UUID,
+        project: ProjectTable,
         db: Session,
     ):
-        ...
+        if project.status != ProcessingStatus.draft:
+            raise HTTPException(
+                status_code=400, detail='Project already started')
 
-    def _notify_listeners(self, task_id: str) -> None:
-        ...
+        task = ProcessingTask(project)
 
-    def _update_task_status(self):
-        ...
+    @staticmethod
+    async def update_db(db: Session, task: ProcessingTask, user_id: UUID):
+        await ProjectRepo.instance.replace_project(db, task.project, user_id)
